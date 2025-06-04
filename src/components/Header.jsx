a0ab9e9727bcb67f8b25764e553e842d07267634
phalcobtw <laxz_16@hotmail.com>
2025-06-03 18:24:05 -0700

    added ref for input values

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
-export default function Header() {
+export default function Header({ ref, onSubmit }) {
   return (
     <header className="header">
       <h1>PhalcoDex</h1>
       <div className="container-search">
-        <input type="text" placeholder="Pokemon..." />
-        <button>Buscar</button>
+        <input type="text" placeholder="Pokemon..." ref={ref} />
+        <button onClick={onSubmit}>Buscar</button>
       </div>
     </header>
   );
