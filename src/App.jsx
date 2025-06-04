5a3a04da790e99914cd3d47de29d5d87453f8598
phalcobtw <laxz_16@hotmail.com>
2025-06-03 18:23:56 -0700

    async api function calls added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import "./App.css";
 import Header from "./components/Header.jsx";
 import PokedexContainer from "./components/PokedexContainer.jsx";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  const inputRef = useRef();
+  const [pokemonData, setPokemonData] = useState({});
+
+  async function fetchPokemonData(pokemon) {
+    try {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function handleClick() {
+    setPokemonData(fetchPokemonData(inputRef.current.value));
+  }
+
   return (
     <>
-      <Header></Header>
-      <PokedexContainer></PokedexContainer>
+      <Header ref={inputRef} onSubmit={handleClick}></Header>
+      <PokedexContainer data={pokemonData}></PokedexContainer>
     </>
   );
 }
