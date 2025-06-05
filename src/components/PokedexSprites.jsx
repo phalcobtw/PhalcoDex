80c2aa3eb7e58c9b4232e3200ad46bb94b9c9a06
phalcobtw <laxz_16@hotmail.com>
2025-06-04 18:31:05 -0700

    fixed a bug that made the app crash when no back sprite exists

--- src/components/PokedexSprites.jsx
+++ src/components/PokedexSprites.jsx
@@ -1,8 +1,8 @@
 export default function PokedexSprites({ data }) {
   return (
     <div className="pokedex-sprites">
-      {data.sprites ? <img src={data.sprites.front_default} alt={data.name} /> : "..."}
-      {data.sprites ? <img src={data.sprites.back_default} alt={data.name} /> : ""}
+      {data?.sprites?.front_default ? <img src={data.sprites.front_default} alt={data.name} /> : "..."}
+      {data?.sprites?.back_default ? <img src={data.sprites.back_default} alt={data.name} /> : ""}
     </div>
   );
 }
