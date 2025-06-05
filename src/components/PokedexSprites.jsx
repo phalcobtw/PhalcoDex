2d3d502588b4f0452b876bd9c236ea7fb1af6e64
phalcobtw <laxz_16@hotmail.com>
2025-06-04 23:51:49 -0700

    increased the size of pokemon sprites

--- src/components/PokedexSprites.jsx
+++ src/components/PokedexSprites.jsx
@@ -1,8 +1,16 @@
 export default function PokedexSprites({ data }) {
   return (
     <div className="pokedex-sprites">
-      {data?.sprites?.front_default ? <img src={data.sprites.front_default} alt={data.name} /> : "..."}
-      {data?.sprites?.back_default ? <img src={data.sprites.back_default} alt={data.name} /> : ""}
+      {data?.sprites?.front_default ? (
+        <img src={data.sprites.front_default} alt={data.name} className="pokemon-sprite" />
+      ) : (
+        "..."
+      )}
+      {data?.sprites?.back_default ? (
+        <img src={data.sprites.back_default} alt={data.name} className="pokemon-sprite" />
+      ) : (
+        ""
+      )}
     </div>
   );
 }
