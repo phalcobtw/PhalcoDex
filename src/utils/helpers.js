7915b5d5c80662d48952ffc2ef625d477dd47502
phalcobtw <laxz_16@hotmail.com>
2025-06-05 22:05:15 -0700

    added filter for HP

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,6 +1,10 @@
 export function capitalizeWord(texto) {
-  return texto
-    .split(/[\s-]/) // divide por espacio o guión
-    .map((palabra) => palabra.charAt(0).toUpperCase() + palabra.slice(1).toLowerCase())
-    .join(" ");
+  if (texto === "hp") {
+    return "HP";
+  } else {
+    return texto
+      .split(/[\s-]/) // divide por espacio o guión
+      .map((palabra) => palabra.charAt(0).toUpperCase() + palabra.slice(1).toLowerCase())
+      .join(" ");
+  }
 }
