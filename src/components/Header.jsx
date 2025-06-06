30c5d7e6dec41b474cdec75dc816b1237937e629
phalcobtw <laxz_16@hotmail.com>
2025-06-05 23:46:38 -0700

    changed placeholder text for input on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ export default function Header({ onSubmit, inputRef }) {
     <header className="header">
       <h1 className="main-title">PhalcoDex</h1>
       <form className="container-search" onSubmit={handleSubmit}>
-        <input type="text" placeholder="Pokemon..." ref={inputRef} />
+        <input type="text" placeholder="e.g. Ampharos / Mewtwo Mega X / Landorus Therian" ref={inputRef} />
         <button type="submit" className="button-component">
           <FontAwesomeIcon icon={faMagnifyingGlass} className="faIcon" />
           <span className="button-text">Buscar</span>
