ebe12320dd1428f705d5b845965dc5eb8a0186de
phalcobtw <laxz_16@hotmail.com>
2025-06-30 18:26:59 -0700

    changed rootlayout and added header with input text

--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -1,12 +1,23 @@
 import Nav from "../components/Nav";
 import DarkBackground from "../components/DarkBackground";
-import { Outlet } from "react-router";
+import { Outlet, useNavigate } from "react-router";
+import Header from "../components/Header";
+import { useRef } from "react";
+import { formatInputText } from "../utils/helpers";
 
 export default function RootLayout() {
+  const inputRef = useRef();
+  const navigate = useNavigate();
+
+  function handleClick() {
+    const name = formatInputText(inputRef.current.value.trim().toLowerCase());
+    navigate(`/pokemon/${name}`);
+  }
   return (
     <>
       <DarkBackground></DarkBackground>
       <Nav></Nav>
+      <Header inputRef={inputRef} onSubmit={handleClick}></Header>
       <Outlet></Outlet>
     </>
   );
