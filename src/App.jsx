61b419f3feda287ce95fd1e14f8bf9fbf9954d36
phalcobtw <laxz_16@hotmail.com>
2025-06-30 17:21:03 -0700

    migrated app content to each individual page using router

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,22 @@
 import "./App.css";
-import Header from "./components/Header.jsx";
-import PokedexContainer from "./components/PokedexContainer.jsx";
-import Toast from "./components/Toast.jsx";
-import DarkBackground from "./components/DarkBackground.jsx";
-import { useEffect, useRef, useState } from "react";
-import { formatInputText } from "./utils/helpers.js";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Home from "./pages/Home.jsx";
+import RootLayout from "./pages/RootLayout.jsx";
+import ErrorPage from "./pages/ErrorPage.jsx";
 
 function App() {
-  const inputRef = useRef();
-  const [toasts, setToasts] = useState([]);
-  const [pokemonData, setPokemonData] = useState({});
-  const [pokemonName, setPokemonName] = useState("");
-
-  useEffect(() => {
-    if (!pokemonName) return; // evita llamada inicial vacía
-
-    async function fetchData() {
-      try {
-        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
-        if (!res.ok) throw new Error("Pokémon no encontrado");
-        const data = await res.json();
-        setPokemonData(data);
-      } catch (err) {
-        showToasty(err.message);
-        setPokemonName("");
-      }
-    }
-
-    fetchData();
-  }, [pokemonName]);
-
-  function showToasty(message) {
-    const id = Date.now(); // ID único por tiempo
-    const newToast = { id, message };
-    setToasts((prev) => [...prev, newToast]);
-  }
-
-  function handleClick() {
-    const name = formatInputText(inputRef.current.value.trim().toLowerCase());
-    if (name) {
-      setPokemonName(name);
-    }
-  }
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <RootLayout></RootLayout>,
+      errorElement: <ErrorPage></ErrorPage>,
+      children: [{ index: true, element: <Home></Home> }],
+    },
+  ]);
 
   return (
     <>
-      <DarkBackground></DarkBackground>
-      <div className="toast-container">
-        {toasts.map((toast) => (
-          <Toast
-            key={toast.id}
-            message={toast.message}
-            duration={3000}
-            onClose={() => setToasts((prev) => prev.filter((t) => t.id !== toast.id))}
-          />
-        ))}
-      </div>
-      <Header inputRef={inputRef} onSubmit={handleClick}></Header>
-      {pokemonData !== null || pokemonData !== undefined ? (
-        <PokedexContainer data={pokemonData}></PokedexContainer>
-      ) : (
-        ""
-      )}
+      <RouterProvider router={router}></RouterProvider>
     </>
   );
 }
