9cf5dc933d7e909d755ad66bb467b9294585cf58
phalcobtw <laxz_16@hotmail.com>
2025-06-30 17:21:12 -0700

    added error page for errorelement on router

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,15 @@
+import DarkBackground from "../components/DarkBackground";
+import Nav from "../components/Nav";
+
+export default function ErrorPage() {
+  return (
+    <>
+      <DarkBackground></DarkBackground>
+      <Nav></Nav>
+      <div className="error-path">
+        <h3>Error!</h3>
+        <p>The route does not exist!</p>
+      </div>
+    </>
+  );
+}
