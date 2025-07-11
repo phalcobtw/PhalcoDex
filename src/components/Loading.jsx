30b50a5826b71711b53a6e021b7b429a837b52a3
phalcobtw <laxz_16@hotmail.com>
2025-07-11 16:37:27 -0700

    added loading component and styles when searching for a pokemon

---
+++ src/components/Loading.jsx
@@ -0,0 +1,14 @@
+import DarkBackground from "../components/DarkBackground";
+import Nav from "../components/Nav";
+
+export default function ErrorPage() {
+  return (
+    <>
+      <DarkBackground></DarkBackground>
+      <div className="loading-container">
+        <h3>Loading!</h3>
+        <p>Please wait a moment...</p>
+      </div>
+    </>
+  );
+}
