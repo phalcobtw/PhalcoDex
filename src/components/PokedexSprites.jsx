fbf0b471b09e617d166f33f0ec85b404344aec4f
phalcobtw <laxz_16@hotmail.com>
2025-07-12 19:42:23 -0700

    logic and icons for hd sprites and shiny sprites

--- src/components/PokedexSprites.jsx
+++ src/components/PokedexSprites.jsx
@@ -1,16 +1,45 @@
+import { useState } from "react";
+import HDIcon from "../assets/hd-display.svg";
+import Sparks from "../assets/sparks-solid.svg";
+
 export default function PokedexSprites({ data }) {
+  const [spriteMode, setSpriteMode] = useState({ hd: false, shiny: false });
+  const sprites = [
+    data?.sprites?.front_default,
+    data?.sprites?.other?.["official-artwork"]?.front_default,
+    data?.sprites?.front_shiny,
+    data?.sprites?.other?.["official-artwork"]?.front_shiny,
+  ];
+
+  function handleClick(identifier) {
+    setSpriteMode((prev) => {
+      return { ...prev, [identifier]: !prev[identifier] };
+    });
+  }
+
+  function getSprite() {
+    const { hd, shiny } = spriteMode;
+    if (hd && shiny) return sprites[3];
+    if (hd) return sprites[1];
+    if (shiny) return sprites[2];
+    return sprites[0];
+  }
+
   return (
     <div className="pokedex-sprites">
-      {data?.sprites?.front_default ? (
-        <img src={data.sprites.front_default} alt={data.name} className="pokemon-sprite" />
-      ) : (
-        "..."
-      )}
-      {data?.sprites?.back_default ? (
-        <img src={data.sprites.back_default} alt={data.name} className="pokemon-sprite" />
-      ) : (
-        ""
-      )}
+      <ul className="sprites-actions">
+        <li>
+          <button onClick={() => handleClick("shiny")} className={spriteMode.shiny ? "active" : ""}>
+            <img src={Sparks}></img>
+          </button>
+        </li>
+        <li>
+          <button onClick={() => handleClick("hd")} className={spriteMode.hd ? "active" : ""}>
+            <img src={HDIcon}></img>
+          </button>
+        </li>
+      </ul>
+      {data?.sprites?.front_default ? <img src={getSprite()} alt={data.name} className="pokemon-sprite" /> : "..."}
     </div>
   );
 }
